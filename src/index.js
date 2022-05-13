f201aaf5bea019c17d01541ba8bdfaa359a147d6
Patrick Dolan <dolanp1992@gmail.com>
2022-05-13 11:27:54 -0700

    add super growing buttons and refactor how buttons are displayed

--- src/index.js
+++ src/index.js
@@ -4,32 +4,47 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { storeState, changeState } from "./js/plant";
 
-// Set state
+// Set stateControl
 
 const stateControl = storeState();
 
-// We create four functions using our function factory. We could easily create many more.
+// Functions for stateControl
 
 const feed = changeState("soil")(1);
-const blueFood = changeState("soil")(5);
+const superFeed = changeState("soil")(5);
 
 const hydrate = changeState("water")(1);
 const superWater = changeState("water")(5);
 
-$(document).ready(function() {
+const giveLight = changeState("light")(1);
+const superLight = changeState("light")(5);
 
-  // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
-
-  $('#feed').click(function() {
-    const newState = stateControl(blueFood);
+$(() => {
+  // Regular grow buttons
+  $('#feed').on("click", () => {
+    const newState = stateControl(feed);
     $('#soil-value').text(`Soil: ${newState.soil}`);
   });
+  $('#water').on("click", () => {
+    const newState = stateControl(hydrate);
+    $('#water-value').text(`Water: ${newState.water}`);
+  });
+  $('#light').on("click", () => {
+    const newState = stateControl(giveLight);
+    $('#light-value').text(`Light: ${newState.light}`);
+  });
 
-  // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
-
-  $('#show-state').click(function() {
-    // We just need to call stateControl() without arguments to see our current state.
-    const currentState = stateControl();
-    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  // Super grow buttons
+  $('#superFeed').on("click", () => {
+    const newState = stateControl(superFeed);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
+  $('#superWater').on("click", () => {
+    const newState = stateControl(superWater);
+    $('#water-value').text(`Water: ${newState.water}`);
+  });
+  $('#superLight').on("click", () => {
+    const newState = stateControl(superLight);
+    $('#light-value').text(`Light: ${newState.light}`);
   });
 });
